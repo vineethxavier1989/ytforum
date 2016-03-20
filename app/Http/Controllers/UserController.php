36a341a9b18f53f69ffa24f39b2f0c9acac84699
vineethxavier1989 <vineethxaviercs3@gmail.com>
2016-03-20 01:52:04 -0500

    first_upload
    downloaded laravel, created database- ytforum, created tables and
    migrated

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+class UserController extends BaseController{
+	//gets the bview for the regoster page
+	public function getCreate()
+	{
+		
+	}
+	// gets the view for the login page
+	public function getLogin()
+	{
+		
+	}
+}
+
+?>
